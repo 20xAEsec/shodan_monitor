303ba55566be0fea4698e472ac89e5c7471be861
Ahmad E. <ahmade171@gmail.com>
2025-02-13 22:32:15 -0800

    Delete credentials.py

--- credentials.py
+++
@@ -1,4 +0,0 @@
-# For security, API Keys are stored in a seperate local file
-# Avoid the hardcoding of sensitive credentials by assigning the api_key to a PATH variable
-# or for a more secure option, retrieve credentials from a secrets manager (AWS Secrets Manager, Azure Key Vault, etc.)
-API_KEY = "<api-key>"
